e0ac3e2b6b0091002072dc8ac7b7041e0be1e3ab
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-08-31 17:32:55 +0200

    type testing

--- app.go
+++ app.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	var name = "Mario"
-	fmt.Print("hello " + name)
+	var investAmount = 1000
+	var returnRate = 3.7
+	var years = 5
+
+	var futureValue = float64(investAmount)*(1 + returnRate / 100)
+	fmt.Print("future value: " + futureValue)
 }
