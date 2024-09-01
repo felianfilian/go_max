4ce0330141f76e872543ddb6d8f3773cbe920fb6
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-09-01 11:00:37 +0200

    type setup

--- app.go
+++ app.go
@@ -6,10 +6,10 @@ import (
 )
 
 func main() {
-	var investAmount = 1000
+	var investAmount float64 = 1000
 	var returnRate = 3.7
-	var years = 5
+	var years float64 = 5
 
-	var futureValue = float64(investAmount)* math.Pow((1 + returnRate / 100), float64(years)) 
+	var futureValue = investAmount * math.Pow((1+returnRate/100), years)
 	fmt.Print(futureValue)
 }
