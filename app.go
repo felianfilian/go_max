a1e7e05c0d2be79bbc331ff4c6f570ace162aa16
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-09-10 16:52:01 +0200

    pointer testing

--- app.go
+++ app.go
@@ -6,6 +6,14 @@ import (
 )
 
 func main() {
+	// Basic tests
+	//BasicTest()
+
+	// Pointers
+	PointerTest()
+}
+
+func BasicTest() {
 	const infaltionRate = 2.5
 	var investAmount float64 = 1000
 	returnRate := 3.7
@@ -17,4 +25,10 @@ func main() {
 	futureValue := investAmount * math.Pow((1+returnRate/100), years)
 	realFutureValue := futureValue / math.Pow(1+infaltionRate/100, years)
 	fmt.Print(realFutureValue)
+}
+
+func PointerTest() {
+	var health = 100
+	var pHealth = &health
+	fmt.Println(pHealth)
 }
